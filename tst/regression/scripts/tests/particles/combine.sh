d93820541e829c5bef0e81a97205af4064c6f3f3
Chao-Chin Yang <ccyang@unlv.edu>
2021-12-17 17:15:44 -0800

    Revise bash script for a particles test.
    Revise combine.sh to accommodate changes in formatted output table by
    commit d6701d6bc.

--- tst/regression/scripts/tests/particles/combine.sh
+++ tst/regression/scripts/tests/particles/combine.sh
@@ -12,12 +12,14 @@ read ntimes < <(ls $BASE.block0.out1.*.par.tab | wc -l)
 (( --ntimes ))
 
 for i in $(seq -f "%05g" 0 $((ntimes - 1))); do
+	dst=$BASE.$i.par.tab
 	if (( nblocks > 1 )); then
-		cat $BASE.block*.out1.$i.par.tab | \
-                sort --general-numeric-sort | \
-		sed "2,${nblocks}d" > $BASE.$i.par.tab
+		grep --regexp="^ *#" $BASE.block0.out1.$i.par.tab > $dst
+		grep --regexp="^ *#" --invert-match --no-filename \
+				$BASE.block*.out1.$i.par.tab | \
+			sort --general-numeric-sort >> $dst
 	else
-		mv $BASE.block0.out1.$i.par.tab $BASE.$i.par.tab
+		mv $BASE.block0.out1.$i.par.tab $dst
 	fi
 	cat $BASE.block*.out1.$i.tab > $BASE.$i.tab
 done
