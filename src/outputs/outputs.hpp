2dfb41073e54cae804eaeda3c9e075174b5ada13
Chao-Chin Yang <ccyang@unlv.edu>
2021-12-20 15:58:22 -0800

    Extract strut OutputParameters to a new header.
    This structure will be shared between class Outputs and class
    ParticlesOutputs.

--- src/outputs/outputs.hpp
+++ src/outputs/outputs.hpp
@@ -18,6 +18,7 @@
 #include "../athena.hpp"
 #include "../particles/particles_output.hpp"
 #include "io_wrapper.hpp"
+#include "output_parameters.hpp"
 
 #ifdef HDF5OUTPUT
 #include <hdf5.h>
@@ -27,34 +28,6 @@
 class Mesh;
 class ParameterInput;
 class Coordinates;
-
-//----------------------------------------------------------------------------------------
-//! \struct OutputParameters
-//! \brief  container for parameters read from `<output>` block in the input file
-
-struct OutputParameters {
-  int block_number;
-  std::string block_name;
-  std::string file_basename;
-  std::string file_id;
-  std::string variable;
-  std::string file_type;
-  std::string data_format;
-  Real next_time, dt;
-  int file_number;
-  bool output_slicex1, output_slicex2, output_slicex3;
-  bool output_sumx1, output_sumx2, output_sumx3;
-  bool include_ghost_zones, cartesian_vector;
-  bool orbital_system_output;
-  int islice, jslice, kslice;
-  Real x1_slice, x2_slice, x3_slice;
-  // TODO(felker): some of the parameters in this class are not initialized in constructor
-  OutputParameters() : block_number(0), next_time(0.0), dt(0.0), file_number(0),
-                       output_slicex1(false),output_slicex2(false),output_slicex3(false),
-                       output_sumx1(false), output_sumx2(false), output_sumx3(false),
-                       include_ghost_zones(false), cartesian_vector(false),
-                       islice(0), jslice(0), kslice(0) {}
-};
 
 //----------------------------------------------------------------------------------------
 //! \struct OutputData
