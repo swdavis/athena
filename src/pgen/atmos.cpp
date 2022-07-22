6dbcb3b9ddc04901cdacc8b824e2d844b4f07ee2
mccbc <cmcclellan1010@gmail.com>
2022-07-22 15:29:31 -0400

    updating initialization to use Trammell powerlaw and column

--- src/pgen/atmos.cpp
+++ src/pgen/atmos.cpp
@@ -49,6 +49,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real kb    = 1.380649e-16;
   Real mp    = 1.6726e-24;
   Real clight = 2.997924589e10;
+  Real sigma_pi = 6.3e-18;// cm2
 
   Real GM   = pin->GetOrAddReal("problem", "GM", 0.);
   Real mmw   = pin->GetOrAddReal("problem", "mmw", 1.); // Mean molecular weight of gas
@@ -62,10 +63,28 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // set up ambient medium at equilibrium for an isothermal atmosphere
   for (int k=ks; k<=ke; k++) {
     for (int j=js; j<=je; j++) {
-      for (int i=is-NGHOST; i<=ie+NGHOST; i++) {
+      Real column = 0.;
+      for (int i=ie+NGHOST; i>=is-NGHOST; --i) {
         Real r = pcoord->x1v(i);
         phydro->u(IDN,k,j,i) = rhobase * exp(GM * mmw * mp / kb / temp * (1./pcoord->x1v(i) - 1./rin));
         Real rho = phydro->u(IDN,k,j,i);
+
+        // Calculate density where n_H = n_p in optically thin limit
+        Real Gamma0 = 1. / (6. * 60. * 60.); // s, photoionization rate coefficient
+        Real alpha = 4.18e-13;
+        Real neq0 = Gamma0 / alpha;
+
+        // Calculate neutral hydrogen column
+        Real n_H = std::pow(0.5 * (std::sqrt(neq0 + 4.*rho/mp) - std::sqrt(neq0)), 2.);
+        Real dr = pcoord->dx1f(i);
+        column += n_H * dr;
+
+        // Calculate ionization rate, initialize ionization fraction scalar
+        Real Gamma = Gamma0 / (1. + std::pow(sigma_pi * column, 1.5)); // Trammell et al 2011, Fig 9 powerlaw
+        pscalars->s(0,k,j,i) = 0.5 * (std::sqrt(Gamma) * std::sqrt(4.*alpha*rho/mp + Gamma) / alpha
+                               - Gamma / alpha) * mp;
+        pscalars->r(0,k,j,i) = pscalars->s(0,k,j,i) / rho;
+
         phydro->u(IM1,k,j,i) = 0.0;
         phydro->u(IM2,k,j,i) = 0.0;
         phydro->u(IM3,k,j,i) = 0.0;
@@ -73,15 +92,6 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
         phydro->u(IEN,k,j,i) += 0.5 * SQR(phydro->u(IM1,k,j,i)) / phydro->u(IDN,k,j,i);
         phydro->u(IEN,k,j,i) += 0.5 * SQR(phydro->u(IM2,k,j,i)) / phydro->u(IDN,k,j,i);
         phydro->u(IEN,k,j,i) += 0.5 * SQR(phydro->u(IM3,k,j,i)) / phydro->u(IDN,k,j,i);
-
-        // Calculate photoionization rate
-        Real Gamma = 1. / (6. * 60. * 60.); // Hardcoded for now
-        Real alpha = 4.18e-13;
-
-        // Set s to be rho times ionization frac (=np * mp)
-        pscalars->s(0,k,j,i) = 0.5 * (std::sqrt(Gamma) * std::sqrt(4.*alpha*rho/mp + Gamma) / alpha
-                               - Gamma / alpha) * mp;
-        pscalars->r(0,k,j,i) = pscalars->s(0,k,j,i) / rho;
       }
     }
   }
@@ -98,14 +108,18 @@ void TrackIonization(MeshBlock *pmb, const Real time, const Real dt,
   Real nR = 1. / alpha / dt;
   for (int k=pmb->ks; k<=pmb->ke; ++k) {
     for (int j=pmb->js; j<=pmb->je; ++j) {
-      for (int i=pmb->is-NGHOST; i<=pmb->ie+NGHOST; ++i) {
+      Real column = 0.;
+      for (int i=pmb->ie+NGHOST; i>=pmb->is-NGHOST; --i) {
         Real rho = prim(IDN,k,j,i);
         Real n_p = cons_scalar(0,k,j,i)/mp;
         Real n_H = rho/mp - n_p;
         Real dr = pmb->pcoord->dx1f(i);
-        Real Gamma = Gamma0 / (1. + std::pow(sigma_pi * n_H*dr, 1.5)); // Trammell et al 2011, Fig 9 powerlaw
-        printf("%g\n", Gamma);
+        column += n_H * dr;
+        Real Gamma = Gamma0 / (1. + std::pow(sigma_pi * column, 1.5)); // Trammell et al 2011, Fig 9 powerlaw
         Real nC = Gamma / alpha;
+        if (time <= 0.) {
+          printf("%g %g %g %g %g %g\n", pmb->pcoord->x1v(i), rho, nC, n_H, cons_scalar(0,k,j,i)/rho, column);
+        }
         cons_scalar(0,k,j,i) = 0.5 * mp * (-(nC + nR) + std::sqrt((nC + nR)*(nC + nR) + 4. * (nC*rho/mp + nR*n_p)));
       }
     }
